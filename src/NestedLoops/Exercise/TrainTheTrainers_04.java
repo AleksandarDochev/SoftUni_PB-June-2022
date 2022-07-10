6db177169dc15e4a140de2622e50b6d8619729e4
Alex <aleksandaredochev@gmail.com>
2022-07-10 17:27:17 +0300

    TrainTheTrainers_04 completed

--- src/NestedLoops/Exercise/TrainTheTrainers_04.java
+++ src/NestedLoops/Exercise/TrainTheTrainers_04.java
@@ -1,7 +1,28 @@
 package NestedLoops.Exercise;
 
+import java.util.Scanner;
+
 public class TrainTheTrainers_04 {
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
 
+        int jury = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+        int countGrades =0;
+        double totalGrades =0;
+
+        while (!input.equals("Finish")){
+            String presentationName = input;
+            double sumCurrentPresentationGrades =0;
+            for (int i =1; i <= jury ; i++){
+                double grade = Double.parseDouble(scanner.nextLine());
+                countGrades++;
+                sumCurrentPresentationGrades += grade;
+            }
+            totalGrades += sumCurrentPresentationGrades;
+            System.out.printf("%s - %.2f.%n",presentationName,sumCurrentPresentationGrades/jury);
+            input= scanner.nextLine();
+        }
+        System.out.printf("Student's final assessment is %.2f.%n",totalGrades/countGrades);
     }
 }
