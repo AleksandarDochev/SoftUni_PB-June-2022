532789db930228ee532ca29f046feb0c93271cf1
Alex <aleksandaredochev@gmail.com>
2022-07-16 14:22:11 +0300

    PremadeCode.java add

---
+++ src/ExamPreparation/PreExam/MovieProfit_02.java
@@ -0,0 +1,13 @@
+package ExamPreparation.PreExam;
+
+import java.util.Scanner;
+
+public class MovieProfit_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String xxxxxxxxxxxx = scanner.nextLine();
+        int xxxxxxxxxxxxxx = Integer.parseInt(scanner.nextLine());
+        double xxxxxxxxxxx = Double.parseDouble(scanner.nextLine());
+
+    }
+}
---
+++ src/PremadeCode.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PremadeCode {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String xxxxxxxxxxxx = scanner.nextLine();
+        int xxxxxxxxxxxxxx = Integer.parseInt(scanner.nextLine());
+        double xxxxxxxxxxx = Double.parseDouble(scanner.nextLine());
+
+    }
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+}
